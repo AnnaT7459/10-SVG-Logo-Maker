7a7d4cb445cd7bb7321b2d85b445f9e3c855cf69
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-11-25 17:28:17 -0600

    moved svg and text tags from circle/triangle/square in shapes.js to use in index.js if statements

--- index.js
+++ index.js
@@ -12,11 +12,22 @@
 // SVG VS Code extension https://marketplace.visualstudio.com/items?itemName=jock.svg
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateLogo = ({ characters, charColor, shape, shapeColor })
+const { Circle, Triangle, Square } = require('./lib/shapes')
 
 function init() {
-    inquirer
+    inquirer  
         .prompt([
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Select a shape for your logo.',
+                choices: ['circle', 'triangle', 'square'],
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'What color would you like your shape to be?',
+            },
             {
                 type: 'input',
                 name: 'characters',
@@ -26,16 +37,6 @@ function init() {
                 type: 'input',
                 name: 'charColor',
                 message: 'What color would you like your text to be?',
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                choices: ['circle', 'triangle', 'square'],
-            },
-            {
-                type: 'input',
-                name: 'shapeColor',
-                message: 'What color would you like your shape to be?'
             }
         ])
 
@@ -48,3 +49,16 @@ function init() {
 }
 
 init();
+
+// Circle
+// `<svg width = "300" height = "200"><circle cx="150" cy="100" r="50" fill="${shapeColor}/><text x = "0" y = "15" fill = ${charColor}>${characters}/text></svg>`
+
+// Triangle
+// `<svg height="300" width="200">
+//       <polygon points="100,20 10,180 190,180" style="fill:"${shapeColor}"/><text x = "0" y = "15" fill = ${this.charColor}>${characters}/text>
+//     </svg>`
+
+// Square
+// `<svg width="300" height="200">
+//     <rect x = "50" y = "20" width = "150" height = "150" style = fill = "${shpaeColor}"/><text x = "0" y = "15" fill = ${charColor}>${characters}/text>
+//   </svg>`
